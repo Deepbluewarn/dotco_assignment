9a8024d2c6698f4047d3c471ddb6180844147727
bluewarn <n5lp97@gmail.com>
2025-03-15 11:33:20 +0000

    getUserInfo 함수 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,33 @@
 import { logout } from '@/actions/auth';
+import { getUserInfo } from '@/actions/user';
+import Main from '@/components/Main';
 import { Box, Button, Flex, Title } from '@mantine/core';
-import { cookies } from 'next/headers';
 import Link from 'next/link';
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const user = cookieStore.get('user_id');
-
+  const user = await getUserInfo(true);
+  
   return (
     <Box m={32}>
       <Title order={1}>금형 주문 관리 시스템</Title>
 
       <Flex gap={8}>
         {
-          typeof user === 'undefined' ? (
+          !user ? (
             <>
               <Button component={Link} href={'/auth/login'}>로그인</Button>
               <Button component={Link} href={'/auth/register'}>회원 가입</Button>
             </>
           ) : (
-            <>
-                <form action={logout}>
-                  <Button type="submit">로그아웃</Button>
-                </form>
-            </>
+            <Flex direction={'column'} gap={8}>
+              <form action={logout}>
+                <Button type="submit">로그아웃</Button>
+              </form>
+
+              <Flex>
+                <Main />
+              </Flex>
+            </Flex>
           )
         }
       </Flex>
